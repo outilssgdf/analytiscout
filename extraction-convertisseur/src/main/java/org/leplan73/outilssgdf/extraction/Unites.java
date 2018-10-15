f84db9fb8300074114ecaa8bce5638652723621a
Sébastien BOUCHEX BELLOMIE <sbouchex@gmail.com>
2018-10-15 21:24:20 +0200

    fichier manquant

---
+++ extraction-convertisseur/src/main/java/org/leplan73/outilssgdf/extraction/Unites.java
@@ -0,0 +1,9 @@
+package org.leplan73.outilssgdf.extraction;
+
+import java.util.HashMap;
+
+public class Unites extends HashMap<String,Unite> {
+
+	private static final long serialVersionUID = 1L;
+
+}

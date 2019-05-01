58cd6e9e04fe9d6c296e9365c0a5bf2eaef7d3ac
Sébastien BOUCHEX BELLOMIE <sbouchex@gmail.com>
2019-05-01 10:32:51 +0200

    Mise à jour de poi en 4.0.1

--- servlet/src/main/java/org/leplan73/outilssgdf/servlet/war/Logger.java
+++ servlet/src/main/java/org/leplan73/outilssgdf/servlet/war/Logger.java
@@ -1,17 +1,17 @@
 package org.leplan73.outilssgdf.servlet.war;
 
-import org.apache.logging.log4j.LogManager;
+import org.slf4j.LoggerFactory;
 
 public class Logger {
 
-	static private org.apache.logging.log4j.Logger logger_;
+	static private org.slf4j.Logger logger_;
 	
 	static public void init()
 	{
-		logger_ = LogManager.getLogger("org.leplan73.outilssgdf.war");
+		logger_ = LoggerFactory.getLogger("org.leplan73.outilssgdf.war");
 	}
 	
-	static public org.apache.logging.log4j.Logger get() {
+	static public org.slf4j.Logger get() {
 		return logger_;
 	}
 }

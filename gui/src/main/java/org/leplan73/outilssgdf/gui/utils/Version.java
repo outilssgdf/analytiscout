78f5ad03cf8c91c2227c1a45d6b5da7d410af052
Sébastien Bouchex Bellomié <sbouchex@gmail.com>
2019-05-08 19:50:23 +0200

    Corrections liées au manifest et à la version

--- gui/src/main/java/org/leplan73/outilssgdf/gui/utils/Version.java
+++ gui/src/main/java/org/leplan73/outilssgdf/gui/utils/Version.java
@@ -6,10 +6,12 @@ public class Version {
 	private int major_;
 	private int minor_;
 	private int subminor_;
+	private String mode_;
 	
 	public int getMajor() { return major_; };
 	public int getMinor() { return minor_; };
 	public int getSubMinor() { return subminor_; };
+	public String getMode() { return mode_; }
 	
 	@Override
 	public String toString()
@@ -28,7 +30,17 @@ public class Version {
 		{
 			v.major_ = Integer.valueOf(parts[0]);
 			v.minor_ = Integer.valueOf(parts[1]);
-			v.subminor_ = Integer.valueOf(parts[2]);
+			
+			String subminor = parts[2];
+			String[] subparts = subminor.split("-");
+			if (subparts.length >= 1)
+			{
+				v.subminor_ = Integer.valueOf(subparts[0]);
+				if (subparts.length == 2)
+				{
+					v.mode_ = subparts[1];
+				}
+			}
 			v.complete_ = true;
 		}
 		return v;

02dfa9945b6d56a17c8c55b03d5ec8fde49b65e2
Sébastien BOUCHEX BELLOMIE <sbouchex@gmail.com>
2019-12-18 09:25:18 +0100

    Changement graphique #32

--- gui/src/main/java/org/leplan73/outilssgdf/gui/utils/Images.java
+++ gui/src/main/java/org/leplan73/outilssgdf/gui/utils/Images.java
@@ -2,13 +2,11 @@ package org.leplan73.outilssgdf.gui.utils;
 
 import java.awt.Image;
 import java.awt.Toolkit;
-import java.util.Random;
 
 import javax.swing.ImageIcon;
 
 public class Images {
 	public final static String ROOT_FOLDER = "org/leplan73/outilssgdf/gui";
-	private final static Random random_ = new Random(5);
 
 	public static Image getIcon() {
 		return Toolkit.getDefaultToolkit()
@@ -20,7 +18,6 @@ public class Images {
 	}
 	
 	public static ImageIcon getImage() {
-		int random = random_.nextInt(5)+1;
-		return new ImageIcon(Images.class.getClassLoader().getResource(ROOT_FOLDER + "/panneau_gauche-"+random+".png"));
+		return new ImageIcon(Images.class.getClassLoader().getResource(ROOT_FOLDER + "/panneau_gauche.png"));
 	}
 }

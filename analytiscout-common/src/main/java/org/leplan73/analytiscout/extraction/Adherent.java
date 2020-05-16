05db10f846b847ef3aaf8fe0de9a8845ea210191
Sébastien BOUCHEX BELLOMIE <sbouchex@gmail.com>
2020-05-16 23:20:40 +0200

    #58 Bug de détection d'âge de compas

--- analytiscout-common/src/main/java/org/leplan73/analytiscout/extraction/Adherent.java
+++ analytiscout-common/src/main/java/org/leplan73/analytiscout/extraction/Adherent.java
@@ -504,6 +504,10 @@ public class Adherent {
 				case Consts.CODE_PIOK:
 					if (ageCamp_ < 14) return false;
 				break;
+				case Consts.CODE_COMPAS_T1T2:
+				case Consts.CODE_COMPAS_T3:
+					if (ageCamp_ < 18) return false;
+				break;
 			}
 		}
 		return true;
